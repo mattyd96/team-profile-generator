cf0656cd12a26d7d1ad3b2279a5a62ccaace8da4
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-02-13 01:57:23 +1030

    added questions for manager and engineer, created recurring prompt for different employees. This can still be made more dry

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //---------------------- Packages needed for this application -------------------------------//
 //local js in utils
 const createHtml = require('./src/createHtml');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 //external packages
 const inquirer = require('inquirer');
@@ -8,15 +12,65 @@ const inquirer = require('inquirer');
 //node native packages
 const fs = require('fs');
 
-
 //---------------------- Array of questions for user input ----------------------------------//
+let employee;
 
-const questions = [
+const managerQs = [
     {
         type: 'input',
-        message: 'What is your Project Title? ',
-        name: 'title'
+        message: `What is the Team Manager's Name? `,
+        name: 'managerName'
     },
+    {
+        type: 'input',
+        message: `What is the Team Manager's employee id? `,
+        name: 'managerId'
+    },
+    {
+        type: 'input',
+        message: `What is the Team Manager's Email Address? `,
+        name: 'managerEmail'
+    },
+    {
+        type: 'input',
+        message: `What is the Team Manager's Office Number? `,
+        name: 'managerOffice'
+    },
+    {
+        type: 'list',
+        message: `Would you like to add another employee or finish? `,
+        name: 'create',
+        choices: ['Engineer', 'Intern', "Finish"]
+    },
+];
+
+const engineerQs = [
+    {
+        type: 'input',
+        message: `What is the Engineer's Name? `,
+        name: 'engineerName'
+    },
+    {
+        type: 'input',
+        message: `What is the Team Engineer's employee id? `,
+        name: 'engineerId'
+    },
+    {
+        type: 'input',
+        message: `What is the Team Engineer's Email Address? `,
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: `What is the Team Engineer's Github username? `,
+        name: 'engineerGithub'
+    },
+    {
+        type: 'list',
+        message: `Would you like to add another employee or finish? `,
+        name: 'create',
+        choices: ['Engineer', 'Intern', "Finish"]
+    }
 ];
 
 //---------------------------------- write File -----------------------------------------//
@@ -32,7 +86,38 @@ const writeDoc = (fileName, data) => {
 
 
 //--------------------------------- Ask Questions -> enquirer -------------------------------//
+employee = 'engineer';
 
-inquirer.prompt(questions).then((response) => {
-    console.log(response);
+inquirer.prompt(managerQs).then((response) => {
+    const {managerName, managerId, managerEmail, managerOffice} = response;
+    const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
+    console.log(manager);
+    if(response.create !== 'Finish') {
+        createAnother(response.create);
+    }
 });
+
+const createAnother = employee => {
+    switch (employee) {
+        case 'Engineer':
+            createEngineer();
+            break;
+        case 'Intern':
+            createEngineer();
+            break;
+        default:
+            break;
+    }
+};
+
+
+const createEngineer = () => {
+    inquirer.prompt(engineerQs).then((response) => {
+        const {engineerName, engineerId, engineerEmail, engineerGithub} = response;
+        const engineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
+        console.log(engineer);
+        if(response.create !== 'Finish') {
+            createAnother(response.create);
+        }
+    });
+}
