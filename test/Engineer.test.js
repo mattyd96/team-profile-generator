3a43e50f57d08f30a5d941370dde0a15a458b15f
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-02-12 17:46:21 +1030

    updated engineeer tests

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,8 +1,10 @@
 const Engineer = require("../lib/Engineer");
 
 describe('Engineer', () => {
+    const engineer = new Engineer('Matthew', '32', 'matthew@gmail.com', 'mattyd96');
+
     it('should initialize correctly', () => {
-        const engineer = new Engineer('Matthew', '32', 'matthew@gmail.com', 'mattyd96');
+        //const engineer = new Engineer('Matthew', '32', 'matthew@gmail.com', 'mattyd96');
 
         expect(engineer.name).toBe('Matthew');
         expect(engineer.id).toBe('32');
@@ -11,7 +13,12 @@ describe('Engineer', () => {
     });
 
     it('get role should return Engineer', () => {
-        const manager = new Engineer('Matthew', '32', 'matthew@gmail.com', '65');
-        expect(manager.getRole()).toBe('Engineer');
+        //const engineer = new Engineer('Matthew', '32', 'matthew@gmail.com', '65');
+        expect(engineer.getRole()).toBe('Engineer');
+    });
+
+    it('get github should return mattyd96', () => {
+        //const engineer = new Engineer('Matthew', '32', 'matthew@gmail.com', '65');
+        expect(engineer.getGithub()).toBe('mattyd96');
     });
 });
